ddb1fc86eb175cfe7ea339c6eac4af74480467a6
Ganesh M <ganesh.muniyandi@gmail.com>
2012-03-25 21:57:38 +0800

    Trailing Zeros
    Program to find number of Trailing Zeros.

---
+++ NTrailingZeros.cc
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  a.cc
+ *
+ *    Description:  Test
+ *
+ *        Version:  1.0
+ *        Created:  01/26/12 12:05:55
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Ganesh Muniyandi (gm), ganesh.muniyandi@gmail.com
+ *        Company:  Open Source
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+unsigned int NTrailingZeros(unsigned int N) {
+    unsigned int result = 0;
+    unsigned int divisor = 5;
+    while(true) {
+        unsigned int t = N / divisor;
+        if (!t) break;
+        result += t;
+        divisor *= 5;
+    }
+    return result;
+}
+
+int main() {
+
+    for(unsigned int i=0; i<1000000000; ++i)
+        std::cout << i << " ==> " << NTrailingZeros(i) << std::endl;
+
+    return 0;
+}
